12ccd55973f738dee7b1b1f8f83c436dda909900
AntoninoCucuzza <cucuzza.antonino@hotmail.com>
2023-11-07 17:20:30 +0100

    add seeder games

--- database/seeders/BoardGamesSeeder.php
+++ database/seeders/BoardGamesSeeder.php
@@ -2,6 +2,7 @@
 
 namespace Database\Seeders;
 
+use App\Models\BoardGames;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 
@@ -12,6 +13,12 @@ class BoardGamesSeeder extends Seeder
      */
     public function run(): void
     {
-        //
+
+        $newGame = new BoardGames();
+        $newGame->title = 'risiko';
+        $newGame->description = 'risichio';
+        $newGame->image = 'rischioso';
+        $newGame->price = '$420';
+        $newGame->save();
     }
 }
