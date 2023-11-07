e400d1f4056a26c796b1580da5ef0baf31d0096c
davide-rullo <davide.rullo01@gmail.com>
2023-11-07 17:16:18 +0100

    add seeder

--- database/seeders/BoardGamesSeeder.php
+++ database/seeders/BoardGamesSeeder.php
@@ -2,6 +2,7 @@
 
 namespace Database\Seeders;
 
+use App\Models\BoardGames;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 
@@ -12,6 +13,10 @@ class BoardGamesSeeder extends Seeder
      */
     public function run(): void
     {
-        //
+        $NewBoardGames = new BoardGames();
+        $NewBoardGames->name = 'Monopoli';
+        $NewBoardGames->description = 'gioco a turni';
+        $NewBoardGames->image = 'a';
+        $NewBoardGames->price = '26';
     }
 }
